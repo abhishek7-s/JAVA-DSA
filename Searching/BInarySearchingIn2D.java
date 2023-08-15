4e83501db77991bb424bfd51ec8ea6e6f0153593
Abhishek <abhisheksharma7124@gmail.com>
2023-08-15 21:19:03 +0530

    Revised binary search in 2D array

--- Searching/BInarySearchingIn2D.java
+++ Searching/BInarySearchingIn2D.java
@@ -5,6 +5,24 @@ import java.util.Arrays;
 public class BInarySearchingIn2D {
 
     static int[] bsIn2D(int[][] arr, int target) {
+
+        /*
+            Binary search for 2d array is done by checking last element of row i.e arr[r][c]
+             if the target is greater then the last element then it will increase row by 1 
+             else it will decrease coloum by 1 as the target will be smaller then last element 
+
+             Note :- array should be sorted in row wise and coloum wise 
+             For eg:-
+
+                        int[][] arr =  {
+                                            { 12, 18, 19 },
+                                            { 15, 18, 21 },
+                                            { 20, 24, 25 },
+                                            { 29, 56, 96 }
+                                        };
+         
+         */
+          
         int r = 0;
         int c = arr.length-1;
 
@@ -26,9 +44,9 @@ public class BInarySearchingIn2D {
                 { 12, 18, 19, 26 },
                 { 15, 18, 21, 23 },
                 { 20, 24, 25, 27 },
-                { 29, 56, 86, 96 }
+                { 29, 56, 96, 97 }
         };
 
-        System.out.println(Arrays.toString(bsIn2D(arr, 96)));
+        System.out.println(Arrays.toString(bsIn2D(arr, 56)));
     }
 }
